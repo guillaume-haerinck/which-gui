8e38dcfbc8d25302f106c016b17867a1fcff7b0b
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-22 23:03:25 +0200

    Add Unreal

---
+++ unreal/README.md
@@ -0,0 +1,1 @@
+# Which UI ? Unreal Engine
---
+++ unreal/WhichGui/Source/WhichGui/WhichGuiGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WhichGuiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHICHGUI_API AWhichGuiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

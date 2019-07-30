50bacfcde4b3e04ff0f11fdf26d19e7c9fba6cce
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-30 22:34:38 +0200

    [1] Start unity UIElements

---
+++ unity/which-gui/Assets/1-papers-please/TitleScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// https://learn.unity.com/tutorial/uielements-first-steps
+
+// To open the window, press :
+// ctrl + shift + T
+
+public class TitleScreen : EditorWindow
+{
+   [MenuItem("TitleScreen/Open _%#T")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<TitleScreen>();
+        window.titleContent = new GUIContent("TitleScreen");
+        window.minSize = new Vector2(250, 50);
+    }
+
+    private void OnEnable()
+    {
+        var root = rootVisualElement;
+        var visualTree = Resources.Load<VisualTreeAsset>("TitleScreen");
+        visualTree.CloneTree(root);
+        root.styleSheets.Add(Resources.Load<StyleSheet>("TitleScreen"));
+    }
+}
--- xaml/README.md
+++ xaml/README.md
@@ -9,3 +9,7 @@ Microsoft blend add margins automatically which can screw up the design
 Timeline mode with keyframes usefull but really far from After Effect (copy-paste multiple keyframes, auto-generate keyframes between a beginning and an ending, change timeline cursor to where the mouse is on click). So it is quicker to edit them by hand.
 
 While you can apply css on any html tag, the options are restricted on XAML which can cause a lot of confusion
+
+Need windows media player to play sounds and video (optional on windows 10, so need to install it)
+
+Need to remember to use "copy always" on each imported assets in order to use them

c9c44c6f52d418b3d3c4d99fafd48fe3cd922dd4
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-07-30 20:32:09 +0200

    [1] Xaml play idle animation when arrival completed

--- xaml/WhichGui/papers-please/PapersPlease.xaml.cs
+++ xaml/WhichGui/papers-please/PapersPlease.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -22,6 +23,12 @@ namespace WhichGui
         public PapersPlease()
         {
             InitializeComponent();
+        }
+
+        private void OnArrivalCompleted(object sender, EventArgs e)
+        {
+            var idle = this.FindResource("Idle") as Storyboard;
+            idle.Begin();
         }
     }
 }
